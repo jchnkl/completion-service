a67afd02647513be96dc7904aa725920a1ffab04
Jochen Keil <jochen.keil@gmail.com>
2015-03-17 21:34:07 +0100

    Rewrite completion-service using rest-service

--- src/Completion/Main.hs
+++ src/Completion/Main.hs
@@ -1,5 +1,29 @@
-import qualified Completion.Config as Completion
-import qualified Completion.Service as Completion
+import Control.Applicative ((<$>))
+import Data.Char (ord)
+import Network.Wai.Handler.Warp (Port)
+import Rest.Service
+import System.Environment
+import Web.Scotty (json)
+import qualified Completion.Engine as E
+import qualified Data.ByteString.Char8 as BS
+
+defaultPort :: Port
+defaultPort = foldr1 (+) $ map ord "completion-service" -- 1880
+
+defaultDictionary :: FilePath
+defaultDictionary = "/usr/share/dict/cracklib-small"
+
+service :: FilePath -> Port -> IO ()
+service fp p = do
+    graph <- E.buildGraph . map BS.unpack . BS.words <$> BS.readFile fp
+    runService p "complete" $ json . flip E.complete graph
 
 main :: IO ()
-main = Completion.service Completion.defaultPort
+main = do
+    args <- getArgs
+    case length args of
+        1 -> service defaultDictionary (readPort args)
+        2 -> service (head . drop 1 $ args) (readPort args)
+        _ -> service defaultDictionary defaultPort
+    where
+    readPort args = read (head args) :: Port
