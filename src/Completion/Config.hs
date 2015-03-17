92735296a95b42efa1944334ec95344390194c6a
Jochen Keil <jochen.keil@gmail.com>
2015-03-17 14:02:48 +0100

    Use a sensible port number

--- src/Completion/Config.hs
+++ src/Completion/Config.hs
@@ -1,9 +1,10 @@
 module Completion.Config where
 
+import Data.Char (ord)
 import Network.Wai.Handler.Warp (Port)
 
 defaultPort :: Port
-defaultPort = 8008
+defaultPort = foldr1 (+) $ map ord "completion-service" -- 1880
 
 defaultDictionary :: FilePath
 defaultDictionary = "/usr/share/dict/cracklib-small"
